6173daa077a68fb6fd1470271a0f21b4600155f1
ZarwalD <mdogminec@gmail.com>
2023-10-02 11:18:25 +0700

    Добавьте файлы проекта.

---
+++ lab-3-p4/lab-3-p4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <clocale>
+
+/**
+ *
+ * @brief Вывод текстовой оценки взависимости от балла в промежутке от 0 до 5
+ * 
+ * @return 0
+ * 
+**/
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int mark;
+	std::cout << "Введите оценку (0 - 5): ";
+	std::cin >> mark;
+	switch (mark)
+	{
+		case 5: std::cout << "Отлично"; break;
+		case 4: std::cout << "Хорошо"; break;
+		case 3: std::cout << "Средне"; break;
+		default: std::cout << "Хуже среднего"; break;
+	}
+}
